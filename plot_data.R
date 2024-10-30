848d2cc058d6a9b7124517ed076781d93d8f77f4
Libby Chrisp <elizabeth.chrisp@bnc.ox.ac.uk>
2024-10-30 11:18:28 +0000

    practise

--- plot_data.R
+++ plot_data.R
@@ -1,26 +1,23 @@
 #Script to plot the logistic growth data
 
-growth_data <- read.csv("???")
+growth_data <- read.csv("experimentt.csv")
 
 install.packages("ggplot2")
+
 library(ggplot2)
 
-ggplot(aes(t,N), data = ???) +
-  
+ggplot(aes(t,N), data = growth_data) +
   geom_point() +
-  
-  xlab("t") +
-  
-  ylab("y") +
-  
+  xlab("Time") +
+  ylab("Population size") +
   theme_bw()
 
-ggplot(aes(t,???), data = growth_data) +
+ggplot(aes(t, N), data = growth_data) +
   
   geom_point() +
   
-  xlab("t") +
+  xlab("Time") +
   
-  ylab("y") +
+  ylab("log Population size") +
   
   scale_y_continuous(trans='log10')
